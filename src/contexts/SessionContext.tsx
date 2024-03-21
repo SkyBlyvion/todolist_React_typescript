d8f42dd3dfa187cd984733fc17e6f0893a2b798f
Lopez Nicolas <richtofen60@gmail.com>
2024-03-21 12:03:30 +0100

    add SessionContext

--- src/contexts/SessionContext.tsx
+++ src/contexts/SessionContext.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode, createContext, useState } from "react";
+import { ReactElement, ReactNode, createContext, useContext, useEffect, useState } from "react";
 import { useAuthContext } from "./AuthContext";
 
 // on définit l'objet du contexte de session
@@ -16,9 +16,43 @@ interface SessionContextProviderProps {
 }
 
 // création du composant contexte
-const SessionContextProvider = ({children}:SessionContextProviderProps):ReactElement => {
+const SessionContextProvider = ({ children }: SessionContextProviderProps): ReactElement => {
     const [inSession, setInSession] = useState<boolean>(false);
-    const {setUserInfo} = useAuthContext();
+    const { setUserInfo } = useAuthContext();
 
     //méthode pour recuperer les informations de l'utilisateur
+    const getUserInfo = async () => {
+        const userString = localStorage.getItem('STORAGE_KEY');
+        if (userString) {
+            const user = JSON.parse(userString);
+            setUserInfo(user);
+            setInSession(true);
+        } else {
+            setInSession(false);
+        }
+    };
+
+    // appel de la méthode pour recuperer les informations de l'utilisateur
+    useEffect(() => {
+        getUserInfo();
+    }, [setUserInfo, inSession]);
+
+    //définition des propriétes du contexte
+    const valueContext: SessionContextType = {
+        inSession,
+        setInSession
+    }
+
+    return <SessionContext.Provider value={valueContext}>{children}</SessionContext.Provider>
 }
+
+//methode pour recuperer le contexte
+const useSessionContext = (): SessionContextType => {
+    const context = useContext(SessionContext);
+    if (!context) {
+        throw new Error('useSessionContext must be used within an SessionContextProvider');
+    }
+    return context;
+}
+
+export { SessionContextProvider, useSessionContext, SessionContext }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
 import { AuthContextProvider } from './contexts/AuthContext.tsx'
+import { SessionContextProvider } from './contexts/SessionContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <SessionContextProvider>
+        <App />
+      </SessionContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 )
